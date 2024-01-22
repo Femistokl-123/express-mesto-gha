5cecd57daff06bdad8a856df1d93e6d302d32256
Femistokl-123 <femistokl123angelyuk@yandex.ru>
2024-01-23 00:26:48 +0300

    express-mesto-gha

--- errors/errors.js
+++
@@ -1,9 +0,0 @@
-const ERR_BAD_REQUEST_CODE = 400;
-const ERR_NOT_FOUND_CODE = 404;
-const ERR_DEFAULT_CODE = 500;
-
-module.exports = {
-  ERR_BAD_REQUEST_CODE,
-  ERR_DEFAULT_CODE,
-  ERR_NOT_FOUND_CODE,
-};
